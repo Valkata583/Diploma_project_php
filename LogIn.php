4054800723fbcba838c83c89bd0a9f82cf1a7ccc
Valkata583 <v.dupinov@gmail.com>
2024-01-22 15:11:29 +0200

    new project

---
+++ LogIn.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LogIn</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <link href="LogInCSS.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+        <h1>Влизане в профил</h1>
+        <div id="LogIn">
+            
+            <form id="LogInForm">
+                <label for="email">E-mail:</label>
+                <input id="email" type="email" name="email">
+                <label for="password">Парола:</label>
+                <input id="password" type="password" name="password">
+                
+                <input id="but" type="button" value="Влизане">
+                <p>Нямаш профил? <a href="Register.html">Регистррай се</a></p>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ Register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <link href="RegisterCSS.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+        <h1>Регистрация</h1>
+        <div id="LogIn">
+            
+            <form id="LogInForm">
+                <label for="email">E-mail:</label>
+                <input id="email" type="email" name="email">
+                <label for="password">Парола:</label>
+                <input id="password" type="password" name="password">
+                <label for="passwordrep">Повтори паролата:</label>
+                <input id="passwordrep" type="password" name="passwordrep">
+                
+                <input id="but" type="button" value="Влизане">
+            </form>
+        </div>
+    </body>
+</html>
